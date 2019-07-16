aae62cadd6255533fd08742778d14e603dc1c746
gitvijayy <7822vijay@gmail.com>
2019-07-15 21:45:09 -0700

    created frontend app

--- djangoreact/djangoreact/settings.py
+++ djangoreact/djangoreact/settings.py
@@ -75,6 +75,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djangoreact.wsgi.application'
 ASGI_APPLICATION = 'djangoreact.routing.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
