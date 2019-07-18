7a0894f89e0607b6fdfcc271e722bd06a616b963
gitvijayy <7822vijay@gmail.com>
2019-07-17 23:34:15 -0700

    splitting front and back end

--- djangoreact/frontend/src/components/App.js
+++ djangoreact/frontend/src/components/App.js
@@ -1,5 +1,5 @@
 require("../../templates/frontend/main.css")
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from '../store'
@@ -9,11 +9,12 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <div className="container">
-            <Chat />
-          </div>
-        </Fragment>
+
+        <div className="container">
+          <Chat />
+
+        </div>
+
       </Provider>
     )
   }
